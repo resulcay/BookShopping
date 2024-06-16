b5f37cf3815010c75c411de16b2734b849e4ea24
resulcay <resulcayop@gmail.com>
2024-06-16 20:35:47 +0300

    Add project files.

---
+++ BookShoppingUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookShoppingUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
